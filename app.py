7513b0002b1e54fabe2d297f9a6b1f45373b8212
DICCE <ooridle@nate.com>
2023-01-16 15:34:02 +0900

    2023-01-16

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ import streamlit as st
 import datetime
 import sqlite3
 import pandas as pd
+import os.path
 
-con =sqlite3.connect('users.db')
+file_path = os.path.dirname(__file__)
+db_file = os.path.join(file_path, 'users.db')
+
+con = sqlite3.connect(db_file)
 cur = con.cursor()
 
 st.subheader('회원가입 폼')
